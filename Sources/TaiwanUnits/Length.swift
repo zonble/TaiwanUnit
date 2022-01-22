f78b69f08debf8d79d04d490db46e1142bb4cfdb
Ethan Huang <eh13@hey.com>
2022-01-22 13:47:33 +0800

    Add `長度單位.鄉民`

--- Sources/TaiwanUnits/Length.swift
+++ Sources/TaiwanUnits/Length.swift
@@ -55,4 +55,9 @@ extension 長度單位 {
         長度單位.astronomicalUnits
     }
 
+    public class var 鄉民: 長度單位 {
+        UnitLength(
+          symbol: UnitLength.centimeters.symbol,
+          converter: UnitConverterLinear(coefficient: 0.3, constant: 0.0))
+    }
 }
